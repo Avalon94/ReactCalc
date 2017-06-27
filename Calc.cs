9a640638d9257d1262a960b2a530f89ba92cceb1
Avalon94 <napitok.94@mail.ru>
2017-06-27 08:10:01 +0300

    Yea

---
+++ Calc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ReactCalc
+{
+    class Calc
+    {
+        double res = 0;
+        public double Sum(double a, double b)//Сумма
+        {
+            res = a + b;
+            Console.Write("Ответ: " + res);
+            
+            return 0;
+        }
+
+        public double Sub(double a, double b)//Разность
+        {
+            res = a - b;
+            Console.Write("Ответ: " + res);
+            
+            return 0;
+        }
+
+        public double Mul(double a, double b)//Умножение
+        {
+            res = a * b;
+            Console.Write("Ответ: " + res);
+            
+            return 0;
+        }
+
+        public double Div(double a, double b)//Деление
+        {
+            if (b == 0)
+            {
+                Console.Write("Ошибка: Деление на ноль!");
+                return 0;
+            }
+            else
+            {
+                res = a / b;
+                Console.Write("Ответ: " + res);
+                
+                return 0;
+            }
+            
+        }
+
+        public double Pow(double a)//Возведение в квадрат
+        {
+            Console.Write("Ответ: " + res);
+            res = a * a;
+            return res;
+        }
+
+        public double Kor(double a)//Корень
+        {
+            if (a <= 0)
+            {
+                Console.WriteLine("Ошибка!");
+                return 0;
+            }
+            else
+            {
+                res = Math.Sqrt(a);
+                Console.Write("Ответ: " + res);
+            }
+
+            return 0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int select;
+            double x = 0;
+            double y = 0;
+            Calc decision = new Calc();
+            Console.WriteLine("\t\tКалькулятор");
+            
+            Console.WriteLine("1.Сложение");
+            Console.WriteLine("2.Вычитание");
+            Console.WriteLine("3.Умножение");
+            Console.WriteLine("4.Деление");
+            Console.WriteLine("5.Возведение в квадрат");
+            Console.WriteLine("6.Квадратный корень");
+
+            Console.WriteLine("Выберите операцию: ");
+            select = Convert.ToInt32(Console.ReadLine());
+            
+            switch (select)
+            {
+                case 1:
+                    
+                        Console.WriteLine("Введите x: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        
+                        Console.WriteLine("Введите y: ");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        
+
+                        decision.Sum(x, y);
+                        break;
+
+                case 2:
+                    
+                        Console.WriteLine("Введите x: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        
+                        Console.WriteLine("Введите y: ");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        
+
+                        decision.Sub(x, y);
+                        break;
+                case 3:
+                    
+                        Console.WriteLine("Введите x: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        
+                        Console.WriteLine("Введите y: ");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        
+
+                        decision.Mul(x, y);
+                        break;
+                case 4:
+                    
+                        Console.WriteLine("Введите x: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        
+                        Console.WriteLine("Введите y: ");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        
+
+                        decision.Div(x, y);
+                        break;
+                case 5:
+                    
+                        Console.WriteLine("Введите x: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        
+
+                        decision.Pow(x);
+                        break;
+
+                case 6:
+                    
+                        Console.WriteLine("Введите x: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        
+
+                        decision.Kor(x);
+                        break;
+
+
+                default:
+                    
+                        Console.WriteLine("Ошибка!");
+                        Console.ReadKey();
+                        break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
